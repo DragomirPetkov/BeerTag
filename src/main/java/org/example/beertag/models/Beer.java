171f601cc92ee886b39d88fd4641c2ddb08c4c1a
dramp <dramppet@abv.bg>
2024-01-04 17:42:05 +0200

    commit validation model

--- src/main/java/org/example/beertag/models/Beer.java
+++ src/main/java/org/example/beertag/models/Beer.java
@@ -1,9 +1,19 @@
 package org.example.beertag.models;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public class Beer{
 
+    @Positive(message = "Id should be positive.")
     private int id;
+
+    @NotNull(message = "Name can't be empty.")
+    @Size(min = 2, max = 20, message = "Name should be between 2 and 20 symbols.")
     private String name;
+
+    @Positive(message = "ABV should be positive.")
     private double abv;
 
     public Beer() {
