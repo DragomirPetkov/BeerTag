63a4183718d250c39247246820facbb9de64356d
dramp <dramppet@abv.bg>
2024-01-04 16:39:07 +0200

    commit model

---
+++ src/main/java/org/example/beertag/models/Beer.java
@@ -0,0 +1,43 @@
+package org.example.beertag.models;
+
+public class Beer{
+
+    private int id;
+    private String name;
+    private double abv;
+
+    public Beer() {
+    }
+
+    public Beer(int id, String name, double abv) {
+        this.id = id;
+        this.name = name;
+        this.abv = abv;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAbv() {
+        return abv;
+    }
+
+    public void setAbv(double abv) {
+        this.abv = abv;
+    }
+
+
+}
