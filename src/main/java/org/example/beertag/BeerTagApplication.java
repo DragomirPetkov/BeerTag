05b15414e97ce6e51f4f802e87e42228bfdeb7f7
dramp <dramppet@abv.bg>
2024-01-05 14:11:42 +0200

    commit

--- src/main/java/org/example/beertag/BeerTagApplication.java
+++ src/main/java/org/example/beertag/BeerTagApplication.java
@@ -2,8 +2,12 @@ package org.example.beertag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
 
 @SpringBootApplication
+
 public class BeerTagApplication {
 
     public static void main(String[] args) {
